084c8de0f43a16976d989f9689675458eff044f5
岸波　雄樹 <you_work_24@icloud.com>
2020-05-15 13:03:29 +0900

    add material-ui-customize

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles( (theme) => ({
-    root: {
-      
-    },
-  }));
+const useStyles = makeStyles( () => (
+    createStyles({
+        "button": {
+            borderColor: '#FFB549',
+            color: '#FFB549',
+            fontWight: 600,
+            marginBottom: '8px',
+            "&:hover": {
+                backgroundColor: '#FFb549',
+                color: '#FFF'
+            }
+        }
+    })
+));
 
 const Answer = (props) => {
-    // const classes = useStyles();
+    const classes = useStyles();
 
     return(
-        <Button variant="contained" color="primary" onClick={()=>props.select(props.content, props.nextId)}>
+        <Button 
+                className={classes.button}
+                variant="outlined" onClick={()=>props.select(props.content, props.nextId)}>
             {props.content}
         </Button>
     ) 
